709302d997e716ab3bb05910336181f613f9a89b
sserges <agounkeserge@gmail.com>
2020-09-07 15:44:02 +0000

    'GET Transactions' endpoint added

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require('../models/Transaction');
+
 // @desc Get all transactions
 // @route GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error'
+    });
+  }
 };
 
 // @desc Add transaction
 // @route POST /api/v1/transactions
 // @access Public
-exports.addTransactions = (req, res, next) => {
+exports.addTransactions = async (req, res, next) => {
   res.send('POST transaction');
 };
 
 // @desc Delete transaction
 // @route DELETE /api/v1/transactions/:id
 // @access Public
-exports.deleteTransactions = (req, res, next) => {
+exports.deleteTransactions = async (req, res, next) => {
   res.send('DELETE transaction');
 };
